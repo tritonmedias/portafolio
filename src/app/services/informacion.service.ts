e60c435a6ac4b6cc0d0e25af62601631bd0c52c4
Antonio Gil <tritonmedias@gmail.com>
2018-05-28 12:23:20 -0430

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,14 +8,32 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nostros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http ) { 
 
-    this.http.get("assets/data/info.pagina.json")
-            .subscribe( data =>{
-              console.log(data.json());
-              this.cargada = true;
-              this.info = data.json();
-            })
+   this.carga_info();
+   this.carga_sobre_nostros();
+
   }
+
+  public carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+    .subscribe( data =>{
+      //console.log(data.json());
+      this.cargada = true;
+      this.info = data.json();
+    })
+  }
+
+  public carga_sobre_nostros(){
+    this.http.get("https://paginaweb-4efdb.firebaseio.com/equipo.json")
+    .subscribe( data =>{
+      console.log(data.json());
+      this.cargada_sobre_nostros = true;
+      this.equipo = data.json();
+    })
+  }
+
 }
