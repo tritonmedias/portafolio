4b0601c236dbebd31998d2c39278d6c68d30cb26
Antonio Gil <tritonmedias@gmail.com>
2018-05-28 22:34:10 -0430

    agregamos firebase a portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,9 +19,10 @@ export class ProductosService {
 
      this.http.get('https://paginaweb-4efdb.firebaseio.com/productos_idx.json')
           .subscribe( res => {
-            
+
              console.log( res.json() );
              this.cargando = false;
+             this.productos = res.json();
           })
    }
 
